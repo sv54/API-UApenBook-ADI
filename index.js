b8497a4d31d64f42538bfdac656d85d697e03f92
Serhii Vidernikov <sv54@alu.ua.es>
2022-10-11 13:58:24 +0200

    index base

--- index.js
+++ index.js
@@ -4,10 +4,24 @@ const app = express();
 app.use(express.json()); 
 
 
-app.get('/', function(req, resp){
-    console.log('Abrimos la pagina Home --> Obtener datos de sesion actual si existe y coleccion de libros')
-    resp.end()
+app.get('/', function(req, res){
+    res.redirect('/books') //redireccionamos a la pagina principal /books
 })
+
+app.get('/books', function(req, res){
+    console.log('Abrimos la pagina Home --> Obtener datos de sesion actual si existe y coleccion de libros')
+    res.end()
+})
+
+app.get('/books/:id', function(req, res){
+    const idLibro = req.params.id
+    
+    console.log('Abrimos la pagina de un libro')
+    res.end()
+})
+
+
+app.post('/')
 
 app.listen(3000, function(){
 	console.log("Servidor arrancado!!!")
