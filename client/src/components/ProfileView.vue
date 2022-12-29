7df9a5dd1c747ec7fdf9a843405e752e775db7e5
Serhii Vidernikov <sv54@alu.ua.es>
2022-12-29 20:22:41 +0100

    Entendiendo como funcionan los componentes

---
+++ client/src/components/ProfileView.vue
@@ -0,0 +1,13 @@
+<script>
+    console.log("Hello World from MainView.vue!")
+</script>
+
+<template>
+    <div class="greetings">
+        <h1 class="green">Hi, here is your profile data!</h1>
+    </div>
+</template>
+
+<style>
+
+</style>
