79eb0559a44e149aa531d763aa3ddfa9eee38ba6
Serhii Vidernikov <sv54@mscloud.ua.es>
2023-01-07 10:46:58 +0100

    Llenando footer de enlaces ficticios

--- client/src/components/FooterPart.vue
+++ client/src/components/FooterPart.vue
@@ -1,15 +1,45 @@
 <script>
-    
+
 </script>
 
 <template>
-    <div class="greetings">
+
+    <div>
+        <hr>
         <br>
-        <a href="/">Politica de privacidad</a>
+
+        <div class="row">
+            <div class="col-6">
+                <a href="/books"><h6>Condiciones de uso</h6></a>
+            </div>
+            <div class="col-6">
+                <a href="/books"><h6>Politica de privacidad</h6></a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6">
+                <a href="/books"><h6>Politica de cookies</h6></a>
+            </div>
+            <div class="col-6">
+                <a href="/books"><h6>Nuestro Blog</h6></a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6">
+                <a href="/books"><h6>Contactar</h6></a>
+            </div>
+            <div class="col-6">
+                <a href="/books"><h6>Sobre UApenBook</h6></a>
+            </div>
+        </div>
         <br>
+        <hr>
     </div>
 </template>
 
-<style>
+<style scoped>
+.row{
+    margin: auto;
+}
 
 </style>
